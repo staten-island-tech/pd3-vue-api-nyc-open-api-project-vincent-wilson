355bfb9a8e4b2b2acd7360e7483c7ebbbfdeee36
wersoam <cowistrash@gmail.com>
2023-04-25 10:11:23 -0400

    api work please

--- vite-project/src/components/arraychart.vue
+++ vite-project/src/components/arraychart.vue
@@ -1,21 +1,30 @@
 <script setup>
 import { ref, onMounted } from "vue";
-const car = ref("");
+import personcard from "../components/personcard.vue";
+const deaths = ref("");
 async function getdata() {
-  let data = await fetch(
-    "https://data.cityofnewyork.us/resource/jb7j-dtam.json"
-  );
-  car.value = data.results;
+  try {
+    let response = await fetch(
+      "https://data.cityofnewyork.us/resource/jb7j-dtam.json"
+    );
+    let data = response.json;
+    deaths.value = data.results;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 onMounted(() => getdata());
 </script>
 
 <template>
-  <div class="card">
-    <h2>
-      holy hell
-      {{ car[1].sex }}
-    </h2>
+  <div>
+    <personcard
+      v-for="(rate, year, cause) in deaths"
+      :key="rate.age_adjusted_death_rate"
+      :year="year.year"
+      :cause="cause.leading_cause"
+    />
   </div>
 </template>
 
